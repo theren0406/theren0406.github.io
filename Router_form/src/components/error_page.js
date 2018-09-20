6152629b73207636875a988ae8f4c9af4c08fd6d
Yun-Fan <theren0406@gmail.com>
2018-09-20 14:01:47 +0800

    corrected cloud className

--- Router_form/src/components/error_page.js
+++ Router_form/src/components/error_page.js
@@ -7,9 +7,9 @@ export default function() {
       <h1>404</h1>
       <p>NOT FOUND</p>
       <Link className="btn btn-custom" to="/">Back To Home</Link>
-      <div class="cloud x1"></div>
-      <div class="cloud x1_5"></div>
-      <div class="cloud x2"></div>
+      <div className="cloud x1"></div>
+      <div className="cloud x1_5"></div>
+      <div className="cloud x2"></div>
     </div>
   );
 }
