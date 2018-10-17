ed1e57634eab9e458eec154a78fa76cac388dc41
Yun-Fan <theren0406@gmail.com>
2018-10-17 12:25:32 +0800

    updated routes in routers.js

--- Vue_stockTrader/src/routes.js
+++ Vue_stockTrader/src/routes.js
@@ -1,21 +1,15 @@
 import Home from './components/Home.vue';
 import Portfolio from './components/portfolio/portfolio.vue';
-// import Stocks from './components/stocks/stocks';
+import Stocks from './components/stocks/stocks.vue';
 
-const Stocks = resolve => {
-  require.ensure(['./components/stocks/stocks.vue'], () => {
-      resolve(require('./components/stocks/stocks.vue'));
-  });
-};
-
-// const User = resolve => {
-//   require.ensure(['./components/user/User.vue'], () => {
-//       resolve(require('./components/user/User.vue'));
-//   }, 'user');
+// const Stocks = resolve => {
+//   require.ensure(['./components/stocks/stocks.vue'], () => {
+//       resolve(require('./components/stocks/stocks.vue'));
+//   });
 // };
 
 export const routes = [
-  { path: 'Vue_stockTrader/', component: Home },
-  { path: 'Vue_stockTrader/portfolio', component: Portfolio },
-  { path: 'Vue_stockTrader/stocks', component: Stocks }
+  { path: '/', component: Home },
+  { path: '/portfolio', component: Portfolio },
+  { path: '/stocks', component: Stocks }
 ];
