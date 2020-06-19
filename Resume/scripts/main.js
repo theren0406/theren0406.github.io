52edb4260fc780e778b1d5b9e5c9b8696bfe2fdb
Yun-Fan <theren0406@gmail.com>
2020-06-19 16:07:12 +0800

    Remove Resume folder

--- Resume/scripts/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$('.modal-btn').click(function() {
-	var title = $(this).data("title");
-	var url = $(this).data("img");
-
-  $('#modal-title').html(title);
-  $('#modal-photo').attr("src", url);
-});
